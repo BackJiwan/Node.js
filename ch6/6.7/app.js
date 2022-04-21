0333ede97e9d93e88b4170fb317de084615ef513
BaekJiwan <bn1357nb@gmail.com>
2022-04-21 21:25:02 +0900

    node js practice express

--- ch6/6.7/app.js
+++ ch6/6.7/app.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 
 app.set('port',3000); //port 이름에 속성 3000부여
+
 app.get('/' , (req,res) => {
     res.send('hello express');
 }); //라우터 설정
+
+app.get('/about', (req,res) => {
+    res.send('hello express about');
+})
 
 app.listen(app.get('port'), () => {
     console.log('익스프레스 서버가 실행됩니다. ');
