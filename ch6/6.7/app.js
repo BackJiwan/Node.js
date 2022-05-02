19f8c460a30cd0d13c222e10b843231937718ab0
BaekJiwan <bn1357nb@gmail.com>
2022-05-02 21:44:07 +0900

    node js practice express

--- ch6/6.7/app.js
+++ ch6/6.7/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.set('port',process.env.PORT || 3000); //port 이름에 속성 3000부여
 app.get('/' , (req,res) => {
-    res.send('hello express');
+    res.sendFile(path.join(__dirname, 'index.html'));
+    //__dirname으로 현재 디렉토리의 경로를 모두 전달하고 그 뒤에 파일이름을 붙이는 형식으로
+    //파일을 지정해 주는 것이다.
 }); //라우터 설정
 
 app.post('/',(req,res) => {
